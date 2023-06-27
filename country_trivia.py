0481d4245e210e4c58fc84d9017ec24b949f6fdd
MJackSol <myah.jsgg@gmail.com>
2023-06-27 18:17:22 +0000

    Recieves data for user input country

--- country_trivia.py
+++ country_trivia.py
@@ -1,1 +1,17 @@
-print("git mob success")
+import requests
+from restcountries import RestCountryApiV2 as rapi
+
+#--game start stuff here--
+
+#--ask user to select a country--
+name = input("Choose a country to be quizzed on: ")
+name.lower()
+name.strip()
+
+#--retrieve data for selected country--
+response = requests.get(f'https://restcountries.com/v3.1/name/{name}?fields=capital,currencies,lang,region')
+response_data = response.json() 
+print(response_data)
+
+#--put data into a database--
+
