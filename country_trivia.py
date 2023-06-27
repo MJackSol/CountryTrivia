d8b8a0fd72ae0e61e37e8e2b9008284788d42ace
MJackSol <myah.jsgg@gmail.com>
2023-06-27 18:34:59 +0000

    fixed currency

--- country_trivia.py
+++ country_trivia.py
@@ -11,7 +11,20 @@ name.strip()
 #--retrieve data for selected country--
 response = requests.get(f'https://restcountries.com/v3.1/name/{name}?fields=capital,currencies,lang,region')
 response_data = response.json() 
-print(response_data)
+#print(response_data)
+
+if response.status_code==200:
+    country=response_data[0]
+    currency_=country["currencies"]
+    for key in currency_:
+        if 'name' in currency_[key]:
+            currency_=currency_[key]['name']
+    capital_=country["capital"][0]
+    region_=country["region"]
+
+    print(f"Currencies: {currency_}")
+    print(f"Capital: {capital_}")
+    print(f"Region: {region_}")
 
 #--put data into a database--
 
